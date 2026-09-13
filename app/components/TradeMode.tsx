ae9a6eb4c46041575f2224bb6b41cc53127fb691
FORGE X <Forgex30@outlook.com>
2026-09-13 18:40:23 +0100

    fix: remount Raydium trader when trade query changes

--- app/components/TradeMode.tsx
+++ app/components/TradeMode.tsx
@@ -7,6 +7,7 @@ import { RaydiumCpmmTrader } from "./RaydiumCpmmTrader";
 export function TradeMode() {
   const searchParams = useSearchParams();
   const pool = searchParams.get("pool") || "";
+  const inputMint = searchParams.get("inputMint") || "";
 
-  return pool ? <RaydiumCpmmTrader /> : <FairLaunchTrader />;
+  return pool ? <RaydiumCpmmTrader key={`${pool}:${inputMint}`} /> : <FairLaunchTrader />;
 }
