b3939635a104cfdd36b2ca894317bdd5d7825ac3
FORGE X <Forgex30@outlook.com>
2026-09-13 16:01:06 +0100

    fix: make trade mode react to pool query changes

--- app/components/TradeMode.tsx
+++ app/components/TradeMode.tsx
@@ -1,22 +1,12 @@
 "use client";
 
-import { useSyncExternalStore } from "react";
+import { useSearchParams } from "next/navigation";
 import { FairLaunchTrader } from "./FairLaunchTrader";
 import { RaydiumCpmmTrader } from "./RaydiumCpmmTrader";
 
-function subscribeToLocation() {
-  return () => {};
-}
-
-function getPoolFromLocation() {
-  return new URLSearchParams(window.location.search).get("pool") || "";
-}
-
-function getServerPool() {
-  return "";
-}
-
 export function TradeMode() {
-  const pool = useSyncExternalStore(subscribeToLocation, getPoolFromLocation, getServerPool);
+  const searchParams = useSearchParams();
+  const pool = searchParams.get("pool") || "";
+
   return pool ? <RaydiumCpmmTrader /> : <FairLaunchTrader />;
 }
